836680bb3fc077321fc085c99b4c06eb4ea082ab
sooj <sooj2023@gmail.com>
2025-07-04 20:44:23 +0900

    WIP: save before switching to etc branch

--- happy-detector/src/main/java/com/soo/happy_detector/HappinessDetector.kt
+++ happy-detector/src/main/java/com/soo/happy_detector/HappinessDetector.kt
@@ -6,10 +6,5 @@ import com.soo.happy_detector.model.HappinessResult
 // 행복 감정 탐지 위한 메인 인터페이스
 
 interface HappinessDetector {
-    suspend fun  initialize() // 초기화
-    suspend fun analyzeFrame(bitmap: Bitmap) : HappinessResult // 비트맵 이미지에서 감정 분석
 
-    fun isInitialized() : Boolean // detector 초기화 여부
-
-    fun release() // 디텍터 리소스 해제
 }
